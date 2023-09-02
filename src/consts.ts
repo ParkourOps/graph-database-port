192b9983b3494c06ecf61b4155d0327c85fc948f
Tej Birring <tej@parkourops.com>
2023-09-02 09:12:17 +0100

    adjust consts to add regex.

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
 export default {
-    MAX_NUM_NODE_LABELS: 1_000,
-    MAX_LEN_NODE_LABEL: 1_000,
-    REGEX_NODE_LABELS: "^[0-9a-zA-Z !#%&*+<=>?@-_|~]+$",
-    REGEX_NODE_PROP_KEYS: "^[a-zA-Z_][a-zA-Z0-9_]+$",
+    MAX_NUM_NODE_LABELS: 5_0,
+    MAX_LEN_NODE_LABEL: 5_0,
+    REGEX_NODE_LABEL: "^[0-9a-zA-Z !#%&*+<=>?@-_|~]+$",
+    REGEX_NODE_PROP_KEY: "^[a-zA-Z_][a-zA-Z0-9_]+$",
+    REGEX_LINK_LABEL: "^[0-9a-zA-Z_]+$",
     MIN_LEN_PROP_ARRAY_VAL: 0,
-    MAX_LEN_PROP_ARRAY_VAL: 1_000,
+    MAX_LEN_PROP_ARRAY_VAL: 5_0,
 } as const;
